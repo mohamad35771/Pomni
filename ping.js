ee31e99ce47b49e21f0f82252cc779a31bfd1942
Virox Store 🔥 <mohamadr0793472@gmail.com>
2026-04-20 01:59:13 +0200

    Update ping.js

--- ping.js
+++ ping.js
@@ -9,7 +9,7 @@ const handler = async (m, { conn }) => {
     title: "𝐒𝐩𝐞𝐞𝐝 / 𝐓𝐞𝐬𝐭",
     body: "𝐓𝐞𝐬𝐭𝐢𝐧𝐠 𝐭𝐡𝐞 𝐛𝐨𝐭'𝐬 𝐬𝐩𝐞𝐞𝐝: 𝐈𝐬 𝐢𝐭 𝐟𝐚𝐬𝐭 𝐨𝐫 𝐧𝐨𝐭?",
     newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      name: 'ma31 ~ Gojo 🕷️',
       jid: '120363225356834044@newsletter'
     },
     big: false
